6b017d65a0664cae111244cae5cd4497471ed1e5
Douglas Selph <douglasselph@gmail.com>
2019-10-04 08:22:35 -0500

    Fix recyclerview item decoration.

--- app/src/main/java/com/fetchrewards/dug/simplesort/ui/common/wrapper/StringWrapper.kt
+++ app/src/main/java/com/fetchrewards/dug/simplesort/ui/common/wrapper/StringWrapper.kt
@@ -3,6 +3,9 @@ package com.fetchrewards.dug.simplesort.ui.common.wrapper
 import android.content.Context
 import androidx.annotation.StringRes
 
+/**
+ * This wrapper is useful once unit tests are developed.
+ */
 class StringWrapper(
     private val context: Context
 ) {
--- app/src/main/java/com/fetchrewards/dug/simplesort/ui/main/MainViewMvcImpl.kt
+++ app/src/main/java/com/fetchrewards/dug/simplesort/ui/main/MainViewMvcImpl.kt
@@ -3,13 +3,14 @@ package com.fetchrewards.dug.simplesort.ui.main
 import android.view.Menu
 import android.view.MenuItem
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fetchrewards.dug.simplesort.R
 import com.fetchrewards.dug.simplesort.app.factory.Factory
 import com.fetchrewards.dug.simplesort.ui.common.mvc.ObservableViewMvcImpl
-import com.fetchrewards.dug.simplesort.ui.common.VerticalSpaceItemDecoration
 import com.fetchrewards.dug.simplesort.ui.main.item.MainItemViewMvc
+
 
 class MainViewMvcImpl(
     factory: Factory,
@@ -20,15 +21,11 @@ class MainViewMvcImpl(
 
     private val listView = findViewById<RecyclerView>(R.id.listView)
     private val adapter = MainListAdapter(factory, this)
-    private val vSpace = context.resources.getDimension(R.dimen.item_gap).toInt()
 
     init {
-        listView.layoutManager = LinearLayoutManager(rootView.context)
-        listView.addItemDecoration(
-            VerticalSpaceItemDecoration(
-                vSpace
-            )
-        )
+        val layoutManager = LinearLayoutManager(rootView.context)
+        listView.layoutManager = layoutManager
+        listView.addItemDecoration(DividerItemDecoration(context, layoutManager.orientation))
         listView.adapter = adapter
     }
 
